ba6ee7264495a15f3f6e6470814d1860023366bd
nilorocha <nilo.rr2@gmail.com>
2021-03-07 21:53:35 -0300

    Created String  array at test class

---
+++ api-collections/src/main/kotlin/one/digitalinnovation/collections/TestArrayString.kt
@@ -0,0 +1,22 @@
+package one.digitalinnovation.collections
+
+fun main(){
+    val nomes = Array(3){""}
+    nomes[0] = "Maria"
+    nomes[1] = "Zazá"
+    nomes[2] = "José"
+    for(nomes in nomes){
+        println(nomes)
+    }
+
+    println("--------------------------")
+    nomes.sort()
+    nomes.forEach { println(it) }
+
+
+    println("--------------------------")
+
+    val nomes2 = arrayOf("Monica", "Ana" , "Julio", "Renata")
+    nomes2.sort()
+    nomes2.forEach { println(it) }
+}
